5b7c0740da7c031d079c253c1c43a6f89cc7d405
Jessh2006 <2025-1-01353@lpu.edu.ph>
2025-09-24 22:45:47 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+        printf("=== Counter-Controlled Loops ===\n");
+
+    // Example 1: FOR loop - print numbers 5 to 10
+    for (int i = 5; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    // Example 2: WHILE loop - sum of first 10 natural numbers
+    int i = 1, sum = 0;
+    while (i <= 10) {
+        sum += i;
+        i++;
+    }
+    printf("Sum = %d\n", sum);
+
+    // Example 3: DO-WHILE loop - print numbers 1 to 5
+    int j = 1;
+    do {
+        printf("%d ", j);
+        j++;
+    } while (j <= 5);
+    printf("\n");
+
+    // Example 4: FOR loop - print multiplication table of 7
+    for (int k = 5; k <= 15; k++) {
+        printf("7 x %d = %d\n", k, 7 * k);
+    }
+
+    // Example 5: WHILE loop - countdown 10 to 1
+    int m = 10;
+    while (m > 0) {
+        printf("%d ", m);
+        m--;
+    }
+    printf("\n");
+    
+    return 0;
+}
